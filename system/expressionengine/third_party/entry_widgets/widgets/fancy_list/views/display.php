c3bbaf5d7e8c4048dada2b097ed92619f0ee2089
Iain Urquhart <iain@jamdigital.co.nz>
2013-05-31 11:34:15 +1200

    remove item_count from output.

--- system/expressionengine/third_party/entry_widgets/widgets/fancy_list/views/display.php
+++ system/expressionengine/third_party/entry_widgets/widgets/fancy_list/views/display.php
@@ -4,7 +4,7 @@
 	{/if}
 	{items}
 		{if item_count == 1}<ul class="fancy_list {bullet_type} {css_classes} {type}">{/if}
-		<li>{item_count} {item}</li>
+		<li>{item}</li>
 		{if item_count == total_items}</ul>{/if}
 	{/items}
 {/if}
